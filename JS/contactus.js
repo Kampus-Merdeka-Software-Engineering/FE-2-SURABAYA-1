efbe868d39426a1f7d77e2ca3d741c7bc5799cd1
MFairuzA <fairuzab@gmail.com>
2023-12-05 16:13:13 +0700

    edit ContactUS

--- JS/contactus.js
+++ JS/contactus.js
@@ -1,7 +1,9 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const form = document.querySelector('form');
+const API_BASE_URL = 'http://localhost:3000';
 
-    form.addEventListener('submit', function (event) {
+document.addEventListener('DOMContentLoaded', () => {
+    const contactForm = document.getElementById('contactForm');
+
+    contactForm.addEventListener('submit', async (event) => {
         event.preventDefault(); // Mencegah form untuk melakukan pengiriman langsung
 
         // Ambil nilai dari input
@@ -18,23 +20,26 @@ document.addEventListener('DOMContentLoaded', function () {
 
             // Di sini, Anda dapat menambahkan logika untuk mengirim data ke server menggunakan AJAX atau metode lainnya.
             // Contoh menggunakan fetch:
-            /*
-            fetch('URL_SERVER', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ firstName, lastName, email, mobileNumber, message }),
-            })
-            .then(response => response.json())
-            .then(data => {
-                console.log('Sukses:', data);
-                // Lakukan tindakan lain setelah pengiriman data berhasil
-            })
-            .catch((error) => {
-                console.error('Error:', error);
-            });
-            */
+            
+            try {
+                const response = await fetch(`${API_BASE_URL}/contact-us`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ firstName, lastName, email, mobileNumber, message }),
+                });
+        
+                if (!response.ok) {
+                    const data = await response.json();
+                    throw new Error(data.error || 'Failed to submit form');
+                }
+        
+                return await response.json();
+            } catch (error) {
+                throw error;
+            }
+            
 
             // Di sini, Anda dapat menambahkan logika untuk menampilkan pesan sukses atau kesalahan kepada pengguna.
         } else {
