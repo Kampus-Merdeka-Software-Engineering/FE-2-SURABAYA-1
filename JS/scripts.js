45465050656c67bee7a07aa284fdc18695288ea5
MFairuzA <fairuzab04@gmail.com>
2023-12-09 17:44:25 +0700

    Pop up Register and logo in title for each page

--- JS/scripts.js
+++ JS/scripts.js
@@ -20,8 +20,37 @@ registerForm.addEventListener('submit', async (event) => {
   
       const data = await response.json();
       console.log(data);
+      const customPopup = document.getElementById('customPopup');
+      const popupContent = document.getElementById('popupContent');
+
+      if (response.ok) {
+          popupContent.innerText = 'Registration successful!';
+          customPopup.style.backgroundColor = '#4CAF50'; 
+          customPopup.style.display = 'block';
+
+          setTimeout(() => {
+              window.location.href = '../HTML/login.html';
+          }, 3000);
+      } else {
+
+          popupContent.innerText = `Registration failed: ${data.message}`;
+          customPopup.style.backgroundColor = '#f44336';
+          customPopup.style.display = 'block';
+
+          setTimeout(() => {
+            customPopup.style.display = 'none';
+        }, 3000);
+      }
     } catch (error) {
-      console.error('Error during registration:', error);
+        console.error('Error during registration:', error);
+  
+        popupContent.innerText = 'Error during registration. Please try again.';
+        customPopup.style.backgroundColor = '#f44336'; 
+        customPopup.style.display = 'block';
+
+        setTimeout(() => {
+          customPopup.style.display = 'none';
+      }, 3000);
     }
   });
   
