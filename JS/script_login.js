f2c090335f7f3e284adb52b9d46abfd62ef06380
MFairuzA <fairuzab04@gmail.com>
2023-12-11 14:49:06 +0700

    Tes Integration BE

--- JS/script_login.js
+++ JS/script_login.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:3000';
+const API_BASE_URL = 'https://be-2-surabaya-1-production.up.railway.app';
 
 document.addEventListener('DOMContentLoaded', () => {
     const loginForm = document.getElementById('loginForm');
